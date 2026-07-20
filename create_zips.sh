b52629046ead2e7b0d57903047324fa4bd98afdb
Karl <kc0bfv@gmail.com>
2026-07-20 14:12:42 -0400

    Work around SharePoint limitation
    Zip extraction on SharePoint is limited to 100 files or 300Mb.  This splits the files up and adds an index.

---
+++ create_zips.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+IND=0
+OUTDIR="../../public"
+
+ls */*.html \
+  | xargs -I {} echo '\"{}\"' \
+  | xargs -L 99 echo \
+  | while IFS= read -r FILES_TO_ZIP; do
+  OUT_NAME=$(printf "spo_%03i.zip" $IND)
+  bash -c "zip -ur $OUTDIR/$OUT_NAME $FILES_TO_ZIP"
+
+  IND=$(($IND + 1))
+done
+
+IND=$(($IND - 1))
+
+echo $IND > "$OUTDIR/LASTINDEX"
+zip -ur "$OUTDIR/spo_index.zip" "$OUTDIR/LASTINDEX"
