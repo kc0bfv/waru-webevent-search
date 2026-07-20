0e912705a22cca73c1ed6a2763999a25e7f28b88
Karl <kc0bfv@gmail.com>
2026-07-20 14:12:27 -0400

    Fix cron script

--- cron_script.sh
+++ cron_script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # cron_script.sh - Intended to be run by cron, updates events and commits changes to repo
 
-nix-shell -p hugo --command "$(dirname "$0")/local_test.sh" && git add site data && git commit -m "Update content" && git push
+cd /home/finity/Code/waru-webevent-search && nix-shell -p hugo --command ./local_test.sh && git add site data && git commit -m "Update content" && git push
